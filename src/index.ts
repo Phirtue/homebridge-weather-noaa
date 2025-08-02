70bd99e5a42c8a4bcd31e239c1bc52d909a40405
Phirtue <KevinKeller@protonmail.ch>
2025-08-02 15:17:04 -0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { API } from 'homebridge';
 import { NOAAWeatherPlatform } from './platform';
 
 export = (api: API) => {
-  api.registerPlatform('NOAAWeather', NOAAWeatherPlatform);
+  api.registerPlatform('homebridge-weather-noaa', 'NOAAWeatherPlatform', NOAAWeatherPlatform);
 };
